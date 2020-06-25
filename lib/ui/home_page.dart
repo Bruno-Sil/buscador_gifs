07f59577b613c9cf0f28722b350c36f445aafd8b
Bruno Sil <deltafid@gmail.com>
2020-06-25 19:40:36 -0300

    buscador_gifs

---
+++ lib/ui/home_page.dart
@@ -0,0 +1,64 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  
+  String _search;
+
+  int _offset = 0;
+
+  Future<Map>_getGifs() async{
+   http.Response response;
+
+  if(_search == null)
+    response = await http.get("https://api.giphy.com/v1/gifs/trending?api_key=L0uKDW4YrlVMwexqz7e4HyEv87weHs9a&limit=20&rating=G");
+  else 
+    response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=L0uKDW4YrlVMwexqz7e4HyEv87weHs9a&q=$_search&limit=20&offset$_offset&rating=G&lang=en");
+
+    return json.decode(response.body);
+  }
+
+  @override
+  void initState(){
+    super.initState();
+
+    _getGifs().then((map) {
+     print(map);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.network("https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif"),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: <Widget>[
+          Padding(
+           padding: EdgeInsets.all(10.0),
+           child: TextField(
+           decoration: InputDecoration(
+             labelText: "Pesquise Aqui!",
+             labelStyle: TextStyle(color: Colors.white),
+             border: OutlineInputBorder()
+           ),
+           style: TextStyle(color: Colors.white, fontSize: 18.0),
+           textAlign: TextAlign.center,
+          ),
+          ),
+        ],
+      ),
+    );
+  }
+}
