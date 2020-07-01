e6927caf2c08a50091f6eabd3c116dcd62e315f5
Bruno Sil <deltafid@gmail.com>
2020-07-01 16:05:52 -0300

    buscador_gifs

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: HomePage(),
     theme: ThemeData(hintColor: Colors.white),
   ));
 }
-
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -2,35 +2,35 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  
   String _search;
 
   int _offset = 0;
 
-  Future<Map>_getGifs() async{
-   http.Response response;
+  Future<Map> _getGifs() async {
+    http.Response response;
 
-  if(_search == null)
-    response = await http.get("https://api.giphy.com/v1/gifs/trending?api_key=L0uKDW4YrlVMwexqz7e4HyEv87weHs9a&limit=20&rating=G");
-  else 
-    response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=L0uKDW4YrlVMwexqz7e4HyEv87weHs9a&q=$_search&limit=20&offset$_offset&rating=G&lang=en");
+    if (_search == null)
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/trending?api_key=L0uKDW4YrlVMwexqz7e4HyEv87weHs9a&limit=20&rating=G");
+    else
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=L0uKDW4YrlVMwexqz7e4HyEv87weHs9a&q=$_search&limit=20&offset$_offset&rating=G&lang=en");
 
     return json.decode(response.body);
   }
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
 
     _getGifs().then((map) {
-     print(map);
+      print(map);
     });
   }
 
@@ -39,26 +39,56 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: Image.network("https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif"),
+        title: Image.network(
+            "https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif"),
         centerTitle: true,
       ),
       backgroundColor: Colors.black,
       body: Column(
         children: <Widget>[
           Padding(
-           padding: EdgeInsets.all(10.0),
-           child: TextField(
-           decoration: InputDecoration(
-             labelText: "Pesquise Aqui!",
-             labelStyle: TextStyle(color: Colors.white),
-             border: OutlineInputBorder()
-           ),
-           style: TextStyle(color: Colors.white, fontSize: 18.0),
-           textAlign: TextAlign.center,
+            padding: EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  labelText: "Pesquise Aqui!",
+                  labelStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder()),
+              style: TextStyle(color: Colors.white, fontSize: 18.0),
+              textAlign: TextAlign.center,
+            ),
           ),
+          Expanded(
+            child: FutureBuilder(
+              future: _getGifs(),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                  case ConnectionState.none:
+                    return Container(
+                      width: 200.0,
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                        strokeWidth: 5.0,
+                      ),
+                    );
+                  default:
+                    if (snapshot.hasError)
+                      return Container();
+                    else
+                      _createGifTable(context, snapshot);
+                }
+              },
+            ),
           ),
         ],
       ),
     );
   }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    
+  }
 }
+
